e79b9a1913a7599806c942b42253e83b1198a0fd
Jackson <jackson1712@hotmail.com>
2023-02-09 19:00:13 -0300

    refactored

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")/
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const uploadConfig = require("./configs/upload");
@@ -31,5 +32,5 @@ app.use( (error, request, response, next) => {
     });
 });
 
-const PORT = 3333;
+const PORT =process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
