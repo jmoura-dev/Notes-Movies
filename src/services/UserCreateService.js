a59e7f9470b6cbf7abe0f1157ec817835a94ed93
Jackson <jackson1712@hotmail.com>
2023-02-13 20:38:56 -0300

    added file to handle exceptions

---
+++ src/services/UserCreateService.js
@@ -0,0 +1,24 @@
+const { hash } = require("bcrypt");
+const AppError = require("../utils/AppError");
+
+class UserCreateService{
+    constructor(userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    async execute({ name, email, password }) {
+        const checkUserExists = await this.userRepository.findByEmail(email);
+
+        if(checkUserExists) {
+            throw new AppError("Este email já está sendo usado.");
+        }
+
+        const hashedPassword = await hash(password, 8)
+        
+        const userCreated = await this.userRepository.create({ name, email, password: hashedPassword });
+
+        return userCreated;
+    }
+}
+
+module.exports = UserCreateService;
---
+++ src/services/userCreateService.spec.js
@@ -0,0 +1,42 @@
+const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
+
+
+describe("UserCreateService", () => {
+    let userRepositoryInMemory = null;
+    let userCreateService = null;
+
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory();
+        userCreateService = new UserCreateService(userRepositoryInMemory);
+    });
+
+    it("user should be create", async () => {
+        const user = {
+            name: "Name Test",
+            email: "email@test.com",
+            password: "123"
+        };
+
+        const userCreated = await userCreateService.execute(user);
+        expect(userCreated).toHaveProperty("id");
+    });
+
+    it("user not should be created with exists email", async () => {
+        const user1 = {
+            name: "User Test 1",
+            email: "user@test.com",
+            password: "123"
+        };
+
+        const user2 = {
+            name: "User Test 2",
+            email: "user@test.com",
+            password: "456"
+        };
+
+        await userCreateService.execute(user1);
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este email já está sendo usado."))
+    })
+})
